63cd82c6886ed19dc9187df3501b56b0b6a3a91e
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-07-30 10:27:44 -0700

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,11 +6,23 @@ import { describe, expect, test } from "vitest";
 
 describe("<App />", () => {
   describe("loading character", () => {
-    test('renders the title "Characters"', async () => {});
-    test("renders a list of 10 characters", async () => {});
+    test('renders the title "Characters"', async () => {
+      render(<App />);
+      expect(screen.getByText("characters")).toBeInTheDocument();
+    });
+    test("renders a list of 10 characters", async () => {
+
+      render(<App />);
+        const characters = screen.getAllByTestId("character-item")
+        expect(characters).toHaveLength(10)
+
+    });
   });
   describe("loading more characters", () => {
-    test('has a "Load More Characters" button', async () => {});
+    test('has a "Load More Characters" button', async () => {
+      render(<App />);
+      expect(screen.getByText("Load More Characters")).toBeInTheDocument();
+    });
     test("clicking load more gets 10 more characters", async () => {});
     test("clicking load more increases the page number", async () => {});
   });
